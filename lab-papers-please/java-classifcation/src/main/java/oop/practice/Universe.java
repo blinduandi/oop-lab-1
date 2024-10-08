a26b09613a5533d5e276e72b607c4fd77c6b0629
blinduandi <andi.blindu04@gmail.com>
2024-10-08 10:43:30 +0300

    Create universe class

---
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/Universe.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Universe {
+    private string name;
+    private ArrayList<Creature> creature = new ArrayList<Creature>();
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+
+}
